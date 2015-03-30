ace937696f1d83dfb4b26c667a0e5a290fd8e77c
VladimirNik <vladimir.nikolaev9@gmail.com>
2015-03-30 11:53:25 +0300

    debugging for Scala tree

--- plugin/src/main/scala/scala/tasty/internal/scalac/phase/TastyPhase.scala
+++ plugin/src/main/scala/scala/tasty/internal/scalac/phase/TastyPhase.scala
@@ -22,6 +22,45 @@ trait TastyPhase {
     override def newPhase(prev: Phase): StdPhase = new StdPhase(prev) {
       override def apply(unit: CompilationUnit) {
         println(s"<=== Tasty phase: ${unit.toString()} ===>")
+        val tree = unit.body
+        
+        var debugCond = false
+        
+        import global.{showRaw, show, ValDef}
+
+        def setDebugCond(tr: Tree) = tr match {
+          case ValDef(_, name, _, _) if name.toString() == "testCodeInNextCases" => debugCond = true
+          case ValDef(_, name, _, _) if name.toString() =="body" => debugCond = false
+          case _ =>
+        }
+
+        def debug(str: String) = if (debugCond) println(str)
+        
+        val traverser = new global.Traverser {
+          override def traverse(tree: Tree) {
+            setDebugCond(tree)
+            debug("")
+            tree match {
+              case _ =>
+                debug(s"showCode(tree): ${showCode(tree)}")
+                debug(s"show(tree): ${show(tree)}")
+                debug(s"showRaw(tree): ${showRaw(tree, printTypes = true)}")
+                if (tree.symbol != null) {
+                  debug(s"showRaw(symbol): ${showRaw(tree.symbol, printTypes = true)}")
+                  debug(s"showRaw(symbol.info): ${showRaw(tree.symbol.info, printTypes = true)}")
+                  debug(s"showRaw(symbol.tpe): ${showRaw(tree.symbol.tpe, printTypes = true)}")
+                }
+                if (tree.tpe != null) {
+                  debug(s"showRaw(tree.tpe): ${showRaw(tree.tpe, printTypes = true)}")
+                }
+                super.traverse(tree)
+            }
+            debug("")
+          }
+        }
+        
+        traverser.traverse(tree)
+        
 //        if (!unit.isJava) {
 //          val tree = unit.body
 //          val picklers = new {
