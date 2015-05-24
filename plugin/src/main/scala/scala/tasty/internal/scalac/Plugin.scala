17c3b7b1a633fba4120e6e307a6226b152894c20
VladimirNik <vladimir.nikolaev9@gmail.com>
2015-05-24 03:00:32 +0300

    Order of superaccessors and patmat changed (patmat <-> superaccessors, superaccessors is first)

--- plugin/src/main/scala/scala/tasty/internal/scalac/Plugin.scala
+++ plugin/src/main/scala/scala/tasty/internal/scalac/Plugin.scala
@@ -12,7 +12,7 @@ import scala.tools.nsc.typechecker.Analyzer
 import scala.tools.nsc.transform.Erasure
 import scala.tasty.internal.scalac.gencode.TastyGenPhase
 
-class Plugin(val global: Global) extends NscPlugin with TastyPhase with TastyGenPhase{
+class Plugin(val global: Global) extends NscPlugin with TastyPhase with TastyGenPhase {
   val name = "tasty"
   val description = """Pickles Scala trees (tasty format).
   For more information visit https://github.com/VladimirNik/tasty"""
@@ -41,5 +41,82 @@ class Plugin(val global: Global) extends NscPlugin with TastyPhase with TastyGen
   }
   ()
 
-  val components = List[NscPluginComponent](TastyComponent)
+  //update the order of phases
+  //superaccessors
+  //println(s"declared fields: ${classOf[Global].getDeclaredFields map {_.getName.trim } filter { _.contains("super") } toList }")
+  //val superaccsField = classOf[Global].getDeclaredField("superAccessors$module")
+  //superaccsField.setAccessible(true)
+  //val superaccs = superaccsField.get(global)
+  val newRunsAfterSup = List("typer")
+  //println(s"declared fields: ${classOf[scala.tools.nsc.Global$superAccessors$].getDeclaredFields map {_.getName.trim } /*filter { _.contains("runsAfter") }*/ toList }")
+  val runsAfterSupField = classOf[scala.tools.nsc.Global$superAccessors$].getDeclaredField("runsAfter")
+  runsAfterSupField.setAccessible(true)
+  runsAfterSupField.set(/*superaccs*/ global.superAccessors, newRunsAfterSup)
+  
+  //patmat
+  val newRunsAfterPat = List("superaccessors")
+  //println(s"declared fields: ${classOf[scala.tools.nsc.Global$superAccessors$].getDeclaredFields map {_.getName.trim } /*filter { _.contains("runsAfter") }*/ toList }")
+  val runsAfterPatField = classOf[scala.tools.nsc.Global$patmat$].getDeclaredField("runsAfter")
+  runsAfterPatField.setAccessible(true)
+  runsAfterPatField.set(global.patmat, newRunsAfterPat)
+  
+  //extensionMethods
+    val newRunsAfterEM = List("patmat")
+  //println(s"declared fields: ${classOf[scala.tools.nsc.Global$superAccessors$].getDeclaredFields map {_.getName.trim } /*filter { _.contains("runsAfter") }*/ toList }")
+  val runsAfterEMField = classOf[scala.tools.nsc.Global$extensionMethods$].getDeclaredField("runsAfter")
+  runsAfterEMField.setAccessible(true)
+  runsAfterEMField.set(global.extensionMethods, newRunsAfterEM)
+
+  val components = List[NscPluginComponent](TastyComponent, AfterPatmatComponent, AfterSAComponent)
+
+  object AfterSAComponent extends {
+    val runsAfter = List("superaccessors")
+    val global: this.global.type = this.global
+  } with NscPluginComponent {
+    override val runsRightAfter = Some("superaccessors")
+    val phaseName = "after-superaccessors"
+    override def description = "pickle tasty trees"
+
+    import global._
+
+    override def newPhase(prev: Phase): StdPhase = new StdPhase(prev) {
+      override def apply(unit: CompilationUnit): Unit = {
+        println("Run after superaccessors!")
+      }
+    }
+  }
+
+  object AfterPatmatComponent extends {
+    val runsAfter = List("patmat")
+    val global: this.global.type = this.global
+  } with NscPluginComponent {
+    override val runsRightAfter = Some("patmat")
+    val phaseName = "after-patmat"
+    override def description = "pickle tasty trees"
+
+    import global._
+
+    override def newPhase(prev: Phase): StdPhase = new StdPhase(prev) {
+      override def apply(unit: CompilationUnit): Unit = {
+        println("Run after patmat!")
+      }
+    }
+  }
+
+  object AfterTyperComponent extends {
+    val runsAfter = List("typer")
+    val global: this.global.type = this.global
+  } with NscPluginComponent {
+    override val runsRightAfter = Some("typer")
+    val phaseName = "after-typer"
+    override def description = "pickle tasty trees"
+
+    import global._
+
+    override def newPhase(prev: Phase): StdPhase = new StdPhase(prev) {
+      override def apply(unit: CompilationUnit): Unit = {
+        println("Run after typer!")
+      }
+    }
+  }
 }
