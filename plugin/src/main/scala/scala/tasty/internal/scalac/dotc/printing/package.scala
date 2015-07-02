0fe2709e007907b56183e2a1aab6ee0706f20b4a
VladimirNik <vladimir.nikolaev9@gmail.com>
2015-07-03 01:22:58 +0300

    dotty printers added

--- plugin/src/main/scala/scala/tasty/internal/scalac/dotc/printing/package.scala
+++ plugin/src/main/scala/scala/tasty/internal/scalac/dotc/printing/package.scala
@@ -1,17 +1,42 @@
 package scala.tasty.internal.scalac.dotc
 
+import util.Chars._
+import core.Names.Name
 import core.StdNames.nme
-//import parsing.{precedence, minPrec, maxPrec, minInfixPrec}
+import core.NameOps._
 
 package object printing {
 
   type Precedence = Int
 
-//  val DotPrec       = parsing.maxPrec
-//  val AndPrec       = parsing.precedence(nme.raw.AMP)
-//  val OrPrec        = parsing.precedence(nme.raw.BAR)
-//  val InfixPrec     = parsing.minInfixPrec
-  val GlobalPrec    = 0 //parsing.minPrec
-//  val TopLevelPrec  = parsing.minPrec - 1
+  def precedence(operator: Name): Int =
+    if (operator eq nme.ERROR) -1
+    else {
+      val firstCh = operator(0)
+      if (isScalaLetter(firstCh)) 1
+      else if (operator.isOpAssignmentName) 0
+      else firstCh match {
+        case '|'             => 2
+        case '^'             => 3
+        case '&'             => 4
+        case '=' | '!'       => 5
+        case '<' | '>'       => 6
+        case ':'             => 7
+        case '+' | '-'       => 8
+        case '*' | '/' | '%' => 9
+        case _               => 10
+      }
+    }
+
+  def minPrec = 0
+  def minInfixPrec = 1
+  def maxPrec = 11
+  
+  val DotPrec       = maxPrec
+  val AndPrec       = precedence(nme.raw.AMP)
+  val OrPrec        = precedence(nme.raw.BAR)
+  val InfixPrec     = minInfixPrec
+  val GlobalPrec    = minPrec
+  val TopLevelPrec  = minPrec - 1
 
 }
