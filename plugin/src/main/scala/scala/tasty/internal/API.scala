ebe8925cbcd12d30aadb5ab5747e8ac3e6e49c70
VladimirNik <vladimir.nikolaev9@gmail.com>
2015-07-18 18:05:34 +0300

    main cake object added (in addition to previous commit

---
+++ plugin/src/main/scala/scala/tasty/internal/API.scala
@@ -0,0 +1,22 @@
+package scala.tasty.internal
+
+import scala.tools.nsc.Global
+import scala.tasty.internal.dotc.ast.TTrees
+import scala.tasty.internal.dotc.core._
+import scala.tasty.internal.dotc.core.tasty._
+
+
+class API(val global: Global) extends TTrees
+                              with TAnnotations
+                              with TConstants
+                              with TContexts
+                              with TDenotations
+                              with TSymbols
+                              with TSymDenotations
+                              with TTypeApplications
+                              with TTypes
+                              with PositionPicklers
+                              with TastyPicklers
+                              with TastyPrinters
+                              with TreeBuffers
+                              with TreePicklers
